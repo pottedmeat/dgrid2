3a78c76c29b0a40226f5279fae1e6c066b0d8148
Neil Roberts <neil@sitepen.com>
2016-12-19 17:00:19 -0600

    linting

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 export const mixin = function(to: any, from: any) {
-    from.collection && (to.collection = from.collection);
-    from.columns && (to.columns = from.columns);
-    from.column && (to.column = from.column);
-    from.item && (to.item = from.item);
-    return to;
-}
+	from.collection && (to.collection = from.collection);
+	from.columns && (to.columns = from.columns);
+	from.column && (to.column = from.column);
+	from.item && (to.item = from.item);
+	return to;
+};
